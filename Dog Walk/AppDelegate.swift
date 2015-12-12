5846a74ee42a4755aecece72fc0db46a21f29fc9
Kerolos <keronakhla@gmail.com>
2015-12-11 22:33:16 -0800

    Add starter Xcode project

---
+++ Dog Walk/AppDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  AppDelegate.swift
+//  Dog Walk
+//
+//  Created by Pietro Rea on 7/17/15.
+//  Copyright © 2015 Razeware. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+  
+  var window: UIWindow?
+  
+  
+  func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+    // Override point for customization after application launch
+    return true
+  }
+  
+}
+
